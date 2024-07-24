61b2f454b1285a6746b5f2fcc1f5c07bcf30d009
Mosangou <mosanisangou@gmail.com>
2024-07-24 15:16:19 +0200

    New version home page

--- src/app/components/compo-home/superficieTotale.tsx
+++ src/app/components/compo-home/superficieTotale.tsx
@@ -9,20 +9,32 @@ export default function superficieTotale({}: Props) {
         <div className='flex flex-row mt-2'>
 
             <div className='flex flex-col mr-2 justify-center items-center'>
-                <div className='w-5  h-16 bg-yellow-950'></div>
+                <div className='w-7 flex flex-col-reverse  h-16 border-2 border-black rounded'>
+                   <div className='w-full  h-1/6 bg-yellow-950 border border-black'></div>
+                   <div className='w-full my-2 text-xs'>10%</div>
+                </div>
                 <div className='text-sm'>Pomme</div>
             </div>
             
             <div className='flex flex-col mr-2 justify-center items-center'>
-                <div className='w-5  h-16 bg-yellow-900'></div>
+                <div className='w-7 flex flex-col-reverse  h-16 border-2 border-black rounded'>
+                   <div className='w-full  h-2/6 bg-yellow-950 border border-black'></div>
+                   <div className='w-full my-2 text-xs'>40%</div>
+                </div>
                 <div className='text-sm'>Manioc</div>
             </div>
             <div className='flex flex-col mr-2 justify-center items-center'>
-                <div className='w-5  h-16 bg-yellow-600'></div>
+                <div className='w-7 flex flex-col-reverse  h-16 border-2 border-black rounded'>
+                   <div className='w-full  h-1/6 bg-yellow-950 border border-black'></div>
+                   <div className='w-full my-2 text-xs'>10%</div>
+                </div>
                 <div className='text-sm'>Soja</div>
             </div>
             <div className='flex flex-col mr-2 justify-center items-center'>
-                <div className='w-5  h-16 bg-yellow-400'></div>
+                <div className='w-7 flex flex-col-reverse  h-16 border-2 border-black rounded'>
+                   <div className='w-full  h-2/6 bg-yellow-950 border border-black'></div>
+                   <div className='w-full my-2 text-xs'>40%</div>
+                </div>
                 <div className='text-sm'>Plantain</div>
             </div>
         </div>
